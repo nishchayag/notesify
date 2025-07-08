5a2a56a1e4ebec22cbe39b5a9915d955397962b0
Nishchay Agarwal <nishchay.agar@gmail.com>
2025-07-08 21:04:19 +0530

    removed github and google provider and added env sample

--- libs/authOptions.ts
+++ libs/authOptions.ts
@@ -1,10 +1,10 @@
 import userModel from "@/models/user.model";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
+
 import connectDB from "./connectDB";
 import bcrypt from "bcryptjs";
 import { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
@@ -53,14 +53,6 @@ export const authOptions: NextAuthOptions = {
           return null;
         }
       },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
