ede614ebe519413568cb77c53e1048b7ef6f82b6
Nishchay Agarwal <nishchay.agar@gmail.com>
2025-07-10 13:44:03 +0530

    reset password page bug fix

--- app/resetpassword/page.tsx
+++ app/resetpassword/page.tsx
@@ -1,7 +1,9 @@
 import { Suspense } from "react";
 import ResetPassword from "@/components/ResetPasswordComp";
 export default function ResetPasswordSuspense() {
-  <Suspense fallback={<div>Loading...</div>}>
-    <ResetPassword />
-  </Suspense>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPassword />
+    </Suspense>
+  );
 }
